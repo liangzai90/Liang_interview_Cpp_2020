ed7a32e03eb83c342288ad525b92f8ae1cd95195
Henry <1073072541@qq.com>
2020-06-07 14:28:25 +0800

    【不会、不理解】

--- 剑指offer/第6章 面试中的各种能力/面试题56（一）：【不会】数组中只出现一次的两个数字.cpp
+++ 剑指offer/第6章 面试中的各种能力/面试题56（一）：【不会】数组中只出现一次的两个数字.cpp
@@ -69,13 +69,49 @@ bool IsBit1(int num, unsigned int indexBit)
 //=======================测试代码
 
 
+// ====================测试代码====================
+void Test(const char* testName, int data[], int length, int expected1, int expected2)
+{
+	if (testName != nullptr)
+		printf("%s begins: ", testName);
+
+	int result1, result2;
+	FindNumsAppearOnce(data, length, &result1, &result2);
+
+	if ((expected1 == result1 && expected2 == result2) ||
+		(expected2 == result1 && expected1 == result2))
+		printf("Passed.\n\n");
+	else
+		printf("Failed.\n\n");
+}
+
+void Test1()
+{
+	int data[] = { 2, 4, 3, 6, 3, 2, 5, 5 };
+	Test("Test1", data, sizeof(data) / sizeof(int), 4, 6);
+}
+
+void Test2()
+{
+	int data[] = { 4, 6 };
+	Test("Test2", data, sizeof(data) / sizeof(int), 4, 6);
+}
+
+void Test3()
+{
+	int data[] = { 4, 6, 1, 1, 1, 1 };
+	Test("Test3", data, sizeof(data) / sizeof(int), 4, 6);
+}
+
 
 
 
 
 int main(int argc, char* argv[])
 {
-	
+	Test1();
+	Test2();
+	Test3();
 
 	cout << endl;
 	system("pause");
