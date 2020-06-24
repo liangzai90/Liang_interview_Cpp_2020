aaf0c4f0c3504b821d4b2e09340a5a5e96dfa28f
Henry <1073072541@qq.com>
2020-06-25 00:16:48 +0800

    约瑟夫环，list模拟【环形链表】

--- 剑指offer/第6章 面试中的各种能力/面试题62：圆圈中最后剩下的数字.cpp
+++ 剑指offer/第6章 面试中的各种能力/面试题62：圆圈中最后剩下的数字.cpp
@@ -15,3 +15,139 @@ https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
 // 面试题62：圆圈中最后剩下的数字
 // 题目：0, 1, …, n-1这n个数字排成一个圆圈，从数字0开始每次从这个圆圈里
 // 删除第m个数字。求出这个圆圈里剩下的最后一个数字。
+
+
+#include <iostream>
+#include <cstdio>
+#include <math.h>
+#include <list>
+#include <vector>
+using namespace std;
+
+//=============方法1：
+// 方法1，用vector，然后失败了！！
+//
+
+
+//===================方法2： 其他容器都不对，这题只能用 list 
+int LastRemaining_Solution1(unsigned int n, unsigned int m)
+{
+	if (n < 1 || m < 1)
+		return -1;
+
+	unsigned int i = 0;
+	list<int> numbers;
+	for (int i = 0; i < n; ++i)
+	{
+		numbers.push_back(i);
+	}
+	cout << "======== Solution1: =========" << endl;
+	list<int>::iterator current = numbers.begin();
+	while (numbers.size() > 1)
+	{
+		for (int i = 1; i < m; ++i)
+		{
+			current++;
+			if (current == numbers.end())
+			{
+				current = numbers.begin();
+			}
+		}
+
+		list<int>::iterator next = ++current;
+		if (next == numbers.end())
+		{
+			next = numbers.begin();
+		}
+		--current;
+		cout << (*current) << ",";
+		numbers.erase(current);
+		current = next;
+	}
+	cout << endl;
+	return *(current);
+}
+
+
+//===============方法3
+int LastRemaining_Solution2(unsigned int n, unsigned int m)
+{
+	if (n < 1 || m < 1)
+		return -1;
+	int last = 0;
+	cout << "============== Solution2: =============" << endl;
+	for (int i = 2; i <= n; i++)
+	{
+		last = (last + m) % i;
+		cout << last;
+	}
+	cout << endl;
+	return last;
+}
+
+
+
+// ====================测试代码====================
+void Test(const char* testName, unsigned int n, unsigned int m, int expected)
+{
+	if (testName != nullptr)
+		printf("%s begins: \n", testName);
+
+	if (LastRemaining_Solution1(n, m) == expected)
+		printf("Solution1 passed.\n");
+	else
+		printf("Solution1 failed.\n");
+
+	if (LastRemaining_Solution2(n, m) == expected)
+		printf("Solution2 passed.\n");
+	else
+		printf("Solution2 failed.\n");
+
+	printf("\n");
+}
+
+void Test1()
+{
+	Test("Test1", 5, 3, 3);
+}
+
+void Test2()
+{
+	Test("Test2", 5, 2, 2);
+}
+
+void Test3()
+{
+	Test("Test3", 6, 7, 4);
+}
+
+void Test4()
+{
+	Test("Test4", 6, 6, 3);
+}
+
+void Test5()
+{
+	Test("Test5", 0, 0, -1);
+}
+
+void Test6()
+{
+	Test("Test6", 4000, 997, 1027);
+}
+
+
+int main(int argc, char* argv[])
+{
+	Test1();
+	Test2();
+	Test3();
+	Test4();
+	Test5();
+	Test6();
+
+
+	system("pause");
+	return 0;
+}
+
